ca06554323fadb27640b945ae720f9cb46823f78
rogertad <50554538+rogertad@users.noreply.github.com>
2021-07-31 14:01:14 -0300

    first commit

---
+++ devaneios/src/main/java/br/lippe/devaneios/devaneios/DevaneiosApplication.java
@@ -0,0 +1,13 @@
+package br.lippe.devaneios.devaneios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevaneiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevaneiosApplication.class, args);
+	}
+
+}
