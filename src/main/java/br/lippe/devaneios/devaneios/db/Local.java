523fd271d15eef4738efd641def01c444bdaa3de
rogertad <50554538+rogertad@users.noreply.github.com>
2021-07-31 14:27:30 -0300

    ajuste 2

---
+++ src/main/java/br/lippe/devaneios/devaneios/db/Local.java
@@ -0,0 +1,55 @@
+package br.lippe.devaneios.devaneios.db;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Local {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String cidade;
+
+    protected Local(){}
+
+    public Local(String nome, String cidade){
+        this.setNome(nome);
+        this.setCidade(cidade);
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public Long getId() {
+        return id;
+    }//ddawewe
+
+    public void setId(Long id) {
+        this.id = id;
+        
+    }
+ 
+    @Override
+    public String toString() {
+        return "eu sou ===>>" + id + " "+nome+" "+cidade;
+    }
+
+
+}
---
+++ src/test/java/br/lippe/devaneios/devaneios/DevaneiosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.lippe.devaneios.devaneios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevaneiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
